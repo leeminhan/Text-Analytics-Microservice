6ff5dd7d6ca13e1454deb98ce97a57c08ef0208a
leeminhan <minhan.lmh@gmail.com>
2021-05-21 23:49:04 +0800

    include es client

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from typing import Optional
 from fastapi import FastAPI
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search
 
 app = FastAPI()
+client = Elasticsearch()
+# configure analyzer
+
+from elasticsearch_dsl import connections
+connections.create_connection(hosts=['localhost'], timeout=20)
 
 @app.get("/")
 def read_root():
