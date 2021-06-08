be232ffb2b329fc5464ace4ff16754e21b8310a4
leeminhan <minhan.lmh@gmail.com>
2021-06-08 21:44:33 +0800

    implementation lexical text similarity with cosine

--- lexical_similarity.py
+++ lexical_similarity.py
@@ -1,31 +1,10 @@
+# importing libraries
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sentence_transformers import SentenceTransformer, util
 from tqdm import tqdm
 model = SentenceTransformer('stsb-roberta-base-v2')
-
-# Two lists of sentences
-sentences1 = ['The cat sits outside',
-            'A man is playing guitar',
-            'The new movie is awesome']
-
-sentences2 = ['The dog plays in the garden',
-            'A woman watches TV',
-            'The new movie is so great']
-
-#Compute embedding for both lists
-embeddings1 = model.encode(sentences1, convert_to_tensor=True)
-embeddings2 = model.encode(sentences2, convert_to_tensor=True)
-
-#Compute cosine-similarities
-cosine_scores = util.pytorch_cos_sim(embeddings1, embeddings2)
-print(cosine_scores)
-
-print("\n========= Demo 1: Example =========")
-#Output the pairs with their score
-for i in tqdm(range(len(sentences1))):
-    print("{} \t\t {} \t\t Score: {:.4f}".format(sentences1[i], sentences2[i], cosine_scores[i][i]))
-
-# ============================================================================================================
-
 
 model_answer_1 = "Turin's theory deuterated acetophenone smells different (from non- deuterated from), \
 it deuterated benzaldehyde smells different (from non- deuterated form), structures of deuterated \
@@ -60,37 +39,39 @@ print("\n========= Demo 2: Comparing Student's Responses to Model Answer =======
 print(f"student response 1 score: {cosine_score_1[0][0]}")
 print(f"student response 2 score: {cosine_score_2[0][0]}")
 
-# ============================================================================================================
+# utility function to evaluate jaccard similarity
+def jaccard_similarity(doc_1, doc_2):
+    a = set(doc_1.split())
+    b = set(doc_2.split())
+    c = a.intersection(b)
+    return float(len(c)) / (len(a) + len(b) - len(c))
 
-# Demo 3: Find out the pairs with the highest cosine similarity scores:
-sentences = ['The cat sits outside',
-            'A man is playing guitar',
-            'I love pasta',
-            'The new movie is awesome',
-            'The cat plays in the garden',
-            'A woman watches TV',
-            'The new movie is so great',
-            'Do you like pizza?']
+# corpus = ['The sun is the largest celestial body in the solar system', 
+#         'The solar system consists of the sun and eight revolving planets', 
+#         'Ra was the Egyptian Sun God', 
+#         'The Pyramids were the pinnacle of Egyptian architecture', 
+#         'The quick brown fox jumps over the lazy dog'
+#     ]
 
-#Compute embeddings
-embeddings = model.encode(sentences, convert_to_tensor=True)
+# defining the corpus
+corpus = [
+    "Turin's theory deuterated acetophenone smells different (from non- deuterated from), \
+    it deuterated benzaldehyde smells different (from non- deuterated form), structures of deuterated \
+    acetophenone and benzaldehyde given.",
+    "Molecules have very different shapes so the lock and key theory suggests they would smell different \
+    but in fact they smell similar. By contrast, diols like ethylere glycol smell completely different to \
+    dithols despite very similar shapes.",
+    "Ethylene glycol and ethane clithicl have very dimilar structure but very different smellsin the lock and \
+    key theory, these two molecules would have very similar smells as both molecules could fit in the same lock. \
+    When atoms were replaced with their ixtopeom moleculea there was a subtle change in smell in the lock and \
+    key theory. This would indicate that the massed the banded atoms does affect smell, as predicted by Turin Timber."
+]
 
-#Compute cosine-similarities for each sentence with each other sentence
-cosine_scores = util.pytorch_cos_sim(embeddings, embeddings)
+# Generate the tf-idf vectors for the corpus
+tfidf = TfidfVectorizer()
+X = tfidf.fit_transform(corpus)
 
-#Find the pairs with the highest cosine similarity scores
-pairs = []
-for i in range(len(cosine_scores)-1):
-    for j in range(i+1, len(cosine_scores)):
-        pairs.append({'index': [i, j], 'score': cosine_scores[i][j]})
-
-#Sort scores in decreasing order
-pairs = sorted(pairs, key=lambda x: x['score'], reverse=True)
-
-print("\n========= Demo 3: Pairs with the highest cosine similarity scores: =========")
-for pair in pairs[0:10]:
-    i, j = pair['index']
-    print("{} \t\t {} \t\t Score: {:.4f}".format(sentences[i], sentences[j], pair['score']))
-
-
-# ============================================================================================================
+# print(f"Jaccard Simiarity: {jaccard_similarity(corpus[0], corpus[1])}")
+cosine_sim_score = cosine_similarity(X, X)
+print(f"\nCosine Similarity Score Matrix: \n{cosine_sim_score}")
+print(f"\nCosine Similarity Score of Teacher against student: \n{cosine_sim_score[0]}")
